33d981e7034a706e109ea5126a92e5c2abeb64df
ycc <2427250114@qq.com>
2016-11-16 12:26:36 +0800

    一个新的HoemActivtiy诞生

---
+++ app/src/main/java/com/ycc/bodyguard/HoemActivity.java
@@ -0,0 +1,18 @@
+package com.ycc.bodyguard;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.ycc.util.ActivityManage;
+
+/**
+ * Created by Administrator on 16-11-16.
+ */
+public class HoemActivity extends ActivityManage{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hoem);
+    }
+}
